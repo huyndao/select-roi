7f9f7ad5496cd309ef7407a889c56a9ca5e4ff29
huyndao <40338813+huyndao@users.noreply.github.com>
2023-07-16 20:42:51 -0400

    reformatted

--- extract_roi.py
+++ extract_roi.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-'''
+"""
 This script takes in a csv file of the following format ['file','x1','x2','y1','y2','label'] and
 outputs the extracted ROI's into the same directory that contains said csv file.
 The ROI images will have the name format as followed:
     {label}_{original_filename}_{x1}_{y1}_{x2}_{y2}.extension
-'''
+"""
 
 import numpy as np
 import pandas as pd
@@ -13,38 +13,40 @@ import argparse
 import cv2
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--dataset", required=True, type=str, help="path to input csv of ROI's")
+ap.add_argument(
+    "-i", "--dataset", required=True, type=str, help="path to input csv of ROI's"
+)
 args = ap.parse_args()
 
 CURDIR = os.path.abspath(os.path.curdir)
 DATASET = os.path.abspath(args.dataset)
 OUTDIR = os.path.split(DATASET)[0]
 
-df = pd.read_table(DATASET, sep=',')
+df = pd.read_table(DATASET, sep=",")
 print(args)
 print(OUTDIR)
 
 for i in df.index:
-    name = df.loc[i, 'file']
-    print('Processing image', f' {name} ({i+1} / {df.shape[0]})')
+    name = df.loc[i, "file"]
+    print("Processing image", f" {name} ({i+1} / {df.shape[0]})")
     try:
         img = cv2.imread(name)
     except Exception as exc:
         print(exc)
         continue
-    x1 = df.loc[i, 'x1']
-    y1 = df.loc[i, 'y1']
-    x2 = df.loc[i, 'x2']
-    y2 = df.loc[i, 'y2']
-    label = df.loc[i, 'label'].replace(' ', '_')
+    x1 = df.loc[i, "x1"]
+    y1 = df.loc[i, "y1"]
+    x2 = df.loc[i, "x2"]
+    y2 = df.loc[i, "y2"]
+    label = df.loc[i, "label"].replace(" ", "_")
     roi_img = img[y1:y2, x1:x2]
 
-    extracted_name, _, extracted_extension = os.path.split(name)[1].partition('.')
-    roi_name = f'{label}_{extracted_name}_{x1}_{y1}_{x2}_{y2}.{extracted_extension}'
-    print(f'Output ROI to {os.path.join(OUTDIR, roi_name)}\n')
-    cv2.imwrite(os.path.join(OUTDIR, roi_name), roi_img) 
-    cv2.imshow('roi', roi_img)
-    if cv2.waitKey(100) & 0xFF == ord('q'):
+    extracted_name, _, extracted_extension = os.path.split(name)[1].partition(".")
+    roi_name = f"{label}_{extracted_name}_{x1}_{y1}_{x2}_{y2}.{extracted_extension}"
+    print(f"Output ROI to {os.path.join(OUTDIR, roi_name)}\n")
+    cv2.imwrite(os.path.join(OUTDIR, roi_name), roi_img)
+    cv2.imshow("roi", roi_img)
+    if cv2.waitKey(100) & 0xFF == ord("q"):
         continue
 
 cv2.destroyAllWindows()
--- select_roi.py
+++ select_roi.py
@@ -17,52 +17,62 @@ import argparse
 import cv2
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--dataset", required=True, type=str, help="path to input directory of images")
+ap.add_argument(
+    "-i", "--dataset", required=True, type=str, help="path to input directory of images"
+)
 args = ap.parse_args()
 
 CURDIR = os.path.abspath(os.path.curdir)
 DATASET = os.path.abspath(args.dataset)
-OUTDIR = os.path.join(CURDIR, os.path.split(DATASET)[1] + '_bbox')
+OUTDIR = os.path.join(CURDIR, os.path.split(DATASET)[1] + "_bbox")
 
 if not os.path.exists(OUTDIR):
     os.makedirs(OUTDIR)
 
-files = glob.glob(f'{DATASET}/**/*', recursive=True)
-regex = re.compile(r'.*\.(jpe?g|png|tiff?|bmp)', flags=re.I)
+files = glob.glob(f"{DATASET}/**/*", recursive=True)
+regex = re.compile(r".*\.(jpe?g|png|tiff?|bmp)", flags=re.I)
 images = list(filter(regex.search, files))
 images.sort()
 
 print(args)
-print(OUTDIR, '\n')
+print(OUTDIR, "\n")
 
-df = pd.DataFrame(columns=['file','x1','y1','x2','y2'])
+df = pd.DataFrame(columns=["file", "x1", "y1", "x2", "y2"])
 
-for (i, image) in enumerate(images, start=1):
-    print('Processing image', os.path.relpath(image), f' ({i} / {len(images)})\n')
+for i, image in enumerate(images, start=1):
+    print("Processing image", os.path.relpath(image), f" ({i} / {len(images)})\n")
     try:
         img = cv2.imread(image)
-        bbox = cv2.selectROIs('bbox', img, showCrosshair=False, fromCenter=False)
+        bbox = cv2.selectROIs("bbox", img, showCrosshair=False, fromCenter=False)
     except Exception as exc:
         print(exc)
         continue
     if len(bbox) == 0:
         continue
-    im = np.empty([bbox.shape[0],1], dtype=object)
+    im = np.empty([bbox.shape[0], 1], dtype=object)
     im[:] = image
-    df = df.append(pd.DataFrame(np.c_[im, bbox], columns=['file','x1','y1','x2','y2']), ignore_index=True)
-    if cv2.waitKey(0) & 0xFF == ord('q'):
+    df = df.append(
+        pd.DataFrame(np.c_[im, bbox], columns=["file", "x1", "y1", "x2", "y2"]),
+        ignore_index=True,
+    )
+    if cv2.waitKey(0) & 0xFF == ord("q"):
         break
 
 cv2.destroyAllWindows()
 
-#cv2.selectROIs returns x,y and w,h; the below is to convert to x1,y1 and x2,y2
-df.loc[:,'x2'] = df.loc[:,'x1'] + df.loc[:,'x2']
-df.loc[:,'y2'] = df.loc[:,'y1'] + df.loc[:,'y2']
+# cv2.selectROIs returns x,y and w,h; the below is to convert to x1,y1 and x2,y2
+df.loc[:, "x2"] = df.loc[:, "x1"] + df.loc[:, "x2"]
+df.loc[:, "y2"] = df.loc[:, "y1"] + df.loc[:, "y2"]
 
-print('\nInput the label of the selected class\n')
+print("\nInput the label of the selected class\n")
 label = input()
-df['label'] = label
+df["label"] = label
 
 print(df)
-print('Output to: ' + os.path.join(os.path.relpath(OUTDIR), f'{label.replace(" ","_")}_bbox.csv'))
-df.to_csv(os.path.join(OUTDIR, f'{label.replace(" ","_")}_bbox.csv'), index=False, header=True)
+print(
+    "Output to: "
+    + os.path.join(os.path.relpath(OUTDIR), f'{label.replace(" ","_")}_bbox.csv')
+)
+df.to_csv(
+    os.path.join(OUTDIR, f'{label.replace(" ","_")}_bbox.csv'), index=False, header=True
+)
